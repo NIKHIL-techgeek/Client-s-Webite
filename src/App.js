46e1eb56a4447c613549a37b1bb755e60ab10bb3
Nikhil Kapoor <kapoornikhil.in@gmail.com>
2022-08-21 21:51:00 -0400

    Installed React router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import MainPage from './components/MainPage'
+import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
 const App = () => {
   return (
-    <div></div>
+   <Router>
+    <Routes>
+      <Route path="/" element={<MainPage/>}/>
+      {/* <Route path="/" element={<MainPage/>}/> */}
+      {/* <Route path="/" element={<MainPage/>}/> */}
+      {/* <Route path="/" element={<MainPage/>}/> */}
+      {/* <Route path="/" element={<MainPage/>}/> */}
+    </Routes>
+   </Router>
   )
 }
 
