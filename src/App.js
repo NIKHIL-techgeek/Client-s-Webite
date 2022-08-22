0d90e510fedb18562ba87ffac4304dfecb194faa
Nikhil Kapoor <kapoornikhil.in@gmail.com>
2022-08-21 22:57:49 -0400

    Working on Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import MainPage from './components/MainPage'
 import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
+
 const App = () => {
   return (
    <Router>
     <Routes>
       <Route path="/" element={<MainPage/>}/>
-      {/* <Route path="/" element={<MainPage/>}/> */}
+      {/* <Route path="/" element={<NavBar/>}/> */}
       {/* <Route path="/" element={<MainPage/>}/> */}
       {/* <Route path="/" element={<MainPage/>}/> */}
       {/* <Route path="/" element={<MainPage/>}/> */}
