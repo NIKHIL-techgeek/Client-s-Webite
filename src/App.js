b9538505d75292469a8f7d6da88eb3b099342f20
Nikhil Kapoor <kapoornikhil.in@gmail.com>
2022-08-22 02:32:41 -0400

    Adjusting icons according to screen size

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import MainPage from './components/MainPage'
 import {BrowserRouter as Router , Routes, Route} from 'react-router-dom';
-import NavBar from './components/NavBar';
+
 
 const App = () => {
   return (
    <Router>
     <Routes>
       <Route path="/" element={<MainPage/>}/>
-      {/* <Route path="/" element={<NavBar/>}/> */}
-      {/* <Route path="/" element={<MainPage/>}/> */}
-      {/* <Route path="/" element={<MainPage/>}/> */}
-      {/* <Route path="/" element={<MainPage/>}/> */}
     </Routes>
    </Router>
   )
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
-import NavBar from './NavBar'
-
+import React ,{useState} from "react";
+import {FaBars , FaTimes } from  'react-icons/fa';
 
 const MainPage = () => {
+  const [nav , setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
   return (
-    <div></div>
-  )
-}
+    <div className="fixed w-full h-[45px] flex justify-end items-center px-4 bg-gray-800 text-gray-300">
+       
+      <ul className="hidden md:flex space-x-10 text-l">
+        <li className="hover:underline hover:text-4xl">Home</li>
+        <li className="hover:underline hover:text-4xl ">About</li>
+        <li className="hover:underline hover:text-4xl ">Contact</li>
+        <li className="hover:underline hover:text-4xl ">Services</li>
+      </ul>
 
-export default MainPage
+      <div  onClick={handleClick} className='md:hidden z-10'>
+            {!nav ? <FaBars/> : <FaTimes/>}
+          </div>
+
+
+      {/* Mobile menu */}
+      <ul className={!nav ? 'hidden' : 'absolute top-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+            <li className='py-6 text-4xl  hover:text-5xl hover:underline hover:text-pink-600'>
+              Home
+              </li>
+              <li className='py-6 text-4xl hover:text-5xl hover:underline hover:text-pink-600'>
+              About
+              </li>
+              <li className='py-6 text-4xl hover:text-5xl hover:underline hover:text-pink-600'>
+              Skills
+              </li>
+              <li className='py-6 text-4xl hover:text-5xl hover:underline hover:text-pink-600'>
+              Contact
+              </li>
+            </ul>
+    </div>
+  )
+};
+
+export default MainPage;
